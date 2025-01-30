6cf48a0725ef1a7751bccb79405d8a9afbf1d5c2
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2025-01-30 12:07:18 -0600

    scripts

--- kubejs/server_scripts/fixes_tweaks/bountry_board.js
+++ kubejs/server_scripts/fixes_tweaks/bountry_board.js
@@ -1,5 +1,5 @@
 /** Bounty board recipes only accept oak. The dev has stated this is intended. https://github.com/ejektaflex/Bountiful/issues/271 */
-if (Platform.isLoaded('compactmachines')) {
+if (Platform.isLoaded('bountyboard')) {
 ServerEvents.recipes(event => {
         event.replaceInput({ id: "bountiful:crafting/bountyboard" }, "minecraft:oak_log", "#minecraft:logs")
         event.replaceInput({ id: "bountiful:crafting/bountyboard" }, "minecraft:oak_planks", "#minecraft:planks")
